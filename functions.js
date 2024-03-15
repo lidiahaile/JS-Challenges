e188fa10025689bb97ed0029525f80312965c3ff
Lidia Haile <lidiahaile@gmail.com>
2024-03-15 17:23:59 +0000

    first challenge

---
+++ functions.js
@@ -0,0 +1,25 @@
+// Challenges
+
+// 1. Write a function that takes a number and returs tru if it is a positive number and false if it is a negative number.
+
+   const isNumberPositve  = (number) => {
+    if (number >= 0)
+    return true;
+    else return false;
+   }
+  console.log (isNumberPositive(-1));
+
+// 2. Write a function that takes a number of days and converts it into an age.
+
+
+//3. Write a function that takes three numbers and returns the largest of the three numbers.
+
+
+
+// 4. Write a function that takes an array of names and returns the last name from the array of names.
+
+
+//5. Write a function that takes an array of numbers and returns true if all of the numbers are positive. It should return false if there are one or more negative numbers in the array.
+
+
+
