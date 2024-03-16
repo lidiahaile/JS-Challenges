3132f4607fb5e1cf2329fe6dff129066d4006394
Lidia Haile <lidiahaile@gmail.com>
2024-03-16 03:31:13 +0000

    4th function

--- functions.js
+++ functions.js
@@ -16,10 +16,11 @@ console.log (isNumberPositve(10));
 // 2. Write a function that takes a number of days and converts it into an age.
 function convertDaysToAge(days){
    console.log(age = days/365);
-}
+   }
+   
+   convertDaysToAge(3650);
+   convertDaysToAge(6570);
 
-convertDaysToAge(3650);
-convertDaysToAge(6570);
 //3. Write a function that takes three numbers and returns the largest of the three numbers.
 function getLargestNumber(a,b,c){
     console.log(Math.max(a,b,c));
@@ -30,9 +31,14 @@ getLargestNumber(6,2,3);
 
 
 // 4. Write a function that takes an array of names and returns the last name from the array of names.
-
+function getLastName (names){
+    if (names.length > 0) {
+        
+        console.log(names[names.length - 1]);
+    }
+}
+   
+getLastName(["Charlie", "Rob","Andy"]);
+getLastName(["Ash", "Stu"]);
 
 //5. Write a function that takes an array of numbers and returns true if all of the numbers are positive. It should return false if there are one or more negative numbers in the array.
-
-
-
